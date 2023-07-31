e73bd96a455c423d4144034283ddda4ebc4cc0d9
Cleriston Lima <87100328+CleristonLima@users.noreply.github.com>
2023-07-31 15:04:18 -0300

    Add files via upload

---
+++ Aula01.Revisao/Aula01.Revisao/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula01.Revisao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Console.WriteLine("Olá");
+            // int X = 10;
+            // Console.Write(X);
+
+
+            // Para trazer o dia de hoje
+
+            DayOfWeek dia = DateTime.Now.DayOfWeek;
+            Console.WriteLine(dia);
+            Console.WriteLine((int)dia);
+
+            Console.ReadLine();
+
+        }
+    }
+}
